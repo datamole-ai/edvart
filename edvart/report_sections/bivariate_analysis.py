datamole-ai/edvart

commit 0ff1fecc6e4802a67cc46026a1b83abf14fec779

feat: exclude swapped column pairs in contingency tables (#131)
Old behavior: columns [a,b,c] specified -> get pairs [(a,b), (b,a),
(a,c), (c,a), (b,c), (c,b)]
New behavior: get pairs ([a,b], [a,c], [b,c])

edvart/report_sections/bivariate_analysis.py
@@ -1,5 +1,6 @@
 """Bivariate analysis section package."""
 
+import itertools
 from enum import IntEnum
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
@@ -826,25 +827,28 @@ class ContingencyTable(Section):
         """
 
         def include_column(col: str) -> bool:
-            return (table_threshold < 0 or df[col].nunique() <= table_threshold) and (
-                not df[col].isnull().all()
+            n_unique = df[col].nunique()
+            return (
+                (table_threshold < 0 or n_unique <= table_threshold)
+                and (not df[col].isnull().all())
+                and n_unique > 1
             )
-
-        def include_column_pair(col_pair: Tuple[str, str]) -> bool:
-            if col_pair[0] == col_pair[1]:
-                return False
-            return include_column(col_pair[0]) and include_column(col_pair[1])
 
         if (columns_x is None) != (columns_y is None):
             raise ValueError("Either both or neither of columns_x, columns_y must be specified.")
         if columns_pairs is None:
             if columns_x is None:
                 if columns is None:
-                    columns = [col for col in df.columns if df[col].nunique() > 1]
-                columns_x = columns
-                columns_y = columns
-            columns_pairs = [(col_x, col_y) for col_x in columns_x for col_y in columns_y]
-        columns_pairs = list(filter(include_column_pair, columns_pairs))
+                    columns = [col for col in df.columns if include_column(col)]
+                columns_pairs = list(itertools.combinations(columns, 2))
+            else:
+                columns_pairs = [
+                    (col_x, col_y)
+                    for (col_x, col_y) in itertools.product(columns_x, columns_y)
+                    # Filter out pairs of columns which contain the same column since
+                    # they make no sense in a contingency table
+                    if col_x != col_y
+                ]
 
         for column1, column2 in columns_pairs:
             ContingencyTable.contingency_table(df, column1, column2)

commit d8b89cd6c43a92d29ab01520836255ed1dd706aa

fix: fix included columns in contingnecy tables
A bug was introduced in #131, which caused columns not to be filtered if the parameter `columns` is specified resulting in useless contingency tables being generated.

To reproduce the previously existing bug:
```python
import edvart
dataset = edvart.example_datasets.dataset_titanic()

report = edvart.Report(dataset).add_bivariate_analysis(columns=['Age', 'Fare', 'Survided', 'Sex']).show()
```
The above example displays contingency tables for numeric columns `Age`, `Fare`, which are numeric and thus contain many unique, resulting in huge & useless contingency tables being displayed.

edvart/report_sections/bivariate_analysis.py
@@ -813,16 +813,22 @@ class ContingencyTable(Section):
             Data based on which to create a contingency table.
         columns : List[str], optional
             Which columns to generate pair-wise contingency tables for.
-            All columns which contain more than 1 unique value are used by default.
-            Columns which contain only null values are always excluded.
+            Columns with more than `table_threshold` unique values are excluded.
+            Columns which contain only null values are excluded.
+            To override the excluded columns, specify `columns_pairs`.
+            Ignored if `columns_x` and `columns_y` or `columns_pairs` is specified.
         columns_x : List[str], optional
             If specified, contingency tables are plotted for each pair
             in the cartesian product of `columns_x` and `columns_y`.
+            Columns with more than `table_threshold` unique values are excluded.
+            Columns which contain only null values are excluded.
             If `columns_x` is specified, then `columns_y` must also be specified.
             Ignored if `columns_pairs` is specified.
         columns_y : List[str], optional
             If specified, contingency tables are plotted for each pair
             in the cartesian product of `columns_x` and `columns_y`.
+            Columns with more than `table_threshold` unique values are excluded.
+            Columns which contain only null values are excluded.
             If `columns_y` is specified, then `columns_x` must also be specified.
             Ignored if `columns_pairs` is specified.
         columns_pairs : List[Tuple[str, str]], optional
@@ -842,10 +848,12 @@ class ContingencyTable(Section):
 
         if (columns_x is None) != (columns_y is None):
             raise ValueError("Either both or neither of columns_x, columns_y must be specified.")
+        if columns is None:
+            columns = list(df.columns)
+        columns = [col for col in df.columns if include_column(col)]
+
         if columns_pairs is None:
             if columns_x is None:
-                if columns is None:
-                    columns = [col for col in df.columns if include_column(col)]
                 columns_pairs = list(itertools.combinations(columns, 2))
             else:
                 columns_pairs = [
@@ -853,7 +861,7 @@ class ContingencyTable(Section):
                     for (col_x, col_y) in itertools.product(columns_x, columns_y)
                     # Filter out pairs of columns which contain the same column since
                     # they make no sense in a contingency table
-                    if col_x != col_y
+                    if col_x != col_y and include_column(col_x) and include_column(col_y)
                 ]
 
         for column1, column2 in columns_pairs: